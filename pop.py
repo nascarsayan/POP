e55eb367c889f4704bfd679a0f82ee24e7903208
Sayan Naskar <nascarsayan@gmail.com>
2017-12-01 21:10:25 +0530

    Deleted useless prints

--- pop.py
+++ pop.py
@@ -1,15 +1,10 @@
 
-# coding: utf-8
-
-# In[7]:
-
 import traceback
 import random
 import math
 import copy
 
 boost = 5
-#depminimax = 5
 n = {"Tree": 7, "Animal": 3, "Native": 2, "Poacher": 2, "Blank": 22, "Tot": 14, "Row": 6, "Turn": 20, "depMiniMax": 1}
 idx = {"Tree": 1, "Animal": 2, "Native": 3, "Poacher": 4, "Blank": 0}
 idxrev = { 1:"Tree", 2:"Animal", 3:"Native", 4:"Poacher", 0:"Blank"}
@@ -18,7 +13,6 @@ hit = {"Animal": {"Native": 25, "Poacher": 50}, "Native": {"Poacher": 10, "Tree"
 blank = {"idx": 0, "health": 0}
 safelevel = 200
 
-# In[8]:
 def printBoard (board):
     bslash1 = '\  '
     bslash2 = ' \ '
@@ -85,10 +79,6 @@ def getNeighbor(board, i, j):
             new_cell.append(cell[x])
     return new_cell
 
-
-# In[9]:
-
-
 def getReach2Cells(board, i, j):
     temp1 = getNeighbor(board,i,j)
     temp2=[]
@@ -121,40 +111,32 @@ def tothealth(board):
                 hlth += (5 * (100 - board[i][j]["health"]))
     return hlth
 
-
 def evaluate(board, isplayer):
     try:
         hlth = tothealth(board)
         if(isplayer):
-            #print "Native's turn"
             score = 0
             for i in range(len(board)):
                 for j in range(len(board[0])):
-                    if(board[i][j]["idx"] == 3): #native identified
-                        #print "Cell %d, %d" % (i, j)
+                    if(board[i][j]["idx"] == 3):
                         cells = getReach2Cells(board, i, j)
                         profit = [0]
                         for cell in cells:
                             if(board[cell[0]][cell[1]]["idx"] == idx["Tree"] and cell[2] == 1): #tree at level 1
                                 score += boost
-                                #print "Score boost due to tree at %d, %d" % (cell[0], cell[1])
+
                             elif(board[cell[0]][cell[1]]["idx"] == idx["Poacher"]): #poacher at any level
                                 score -= hit["Poacher"]["Native"]
-                                #print "Score decreased by %d due to poacher at %d, %d" % (hit["Poacher"]["Native"], cell[0], cell[1])
+
                                 temp = board[cell[0]][cell[1]]["health"]/hit["Native"]["Poacher"] #strikes remaining
                                 profit.append(health["Poacher"]/temp)
-                                #print "Score might be increased by %d due to poacher there" % (health["Poacher"]/temp)
+
                             elif(board[cell[0]][cell[1]]["idx"]== idx["Animal"] and cell[2]==1): #animal at level 1
                                 score -= hit["Animal"]["Native"]
-                                #print "cell = %r" % cell
-                                #print "Score might be decreased by %d due to animal at %d, %d" % (hit["Animal"]["Native"], cell[0], cell[1])
                         temp = max(profit)
-                        #print "Max profit = %d" % temp
                         score += (temp + hlth)
-                        #print "Score = %d" % score
 
         else:
-            #print "Poacher's turn"
             score = 0
             for i in range(len(board)):
                 for j in range(len(board[0])):
@@ -165,25 +147,18 @@ def evaluate(board, isplayer):
                             if(board[cell[0]][cell[1]]["idx"]== idx["Tree"] and cell[2]==1): #tree at level 1
                                 temp = board[cell[0]][cell[1]]["health"]/hit["Poacher"]["Tree"] #strikes remaining
                                 profit.append(100 * health["Tree"]/temp)
-                                #print "Score might be increased by %d due to attack on plant at %d, %d" % (health["Tree"]/temp, cell[0], cell[1])
                             elif(board[cell[0]][cell[1]]["idx"]== 2 and cell[2]==2): #animal at level 2
                                 temp = board[cell[0]][cell[1]]["health"]/hit["Poacher"]["Animal"] #strikes remaining
                                 profit.append(100 * health["Animal"]/temp)
-                                #print "Score might be increased by %d due to attack on animal at %d, %d" % (health["Animal"]/temp, cell[0], cell[1])
                             elif(board[cell[0]][cell[1]]["idx"]== 2 and cell[2]==1): #animal at level 1
                                 score -= hit["Animal"]["Poacher"]
-                                #print "Score decreased by %d due to animal at %d, %d" % (hit["Animal"]["Poacher"], cell[0], cell[1])
                             elif(board[cell[0]][cell[1]]["idx"]== 3): #native at any level
                                 score -= hit["Native"]["Poacher"]
-                                #print "Score decreased by %d due to native at %d, %d" % (hit["Native"]["Poacher"], cell[0], cell[1])
                                 temp = board[cell[0]][cell[1]]["health"]/hit["Poacher"]["Native"] #strikes remaining
                                 profit.append(100 * health["Native"]/temp)
-                                #print "Score might be increased by %d due to native there" % (health["Native"]/temp)
                         temp = max(profit)
-                        #print "Max profit = %d" % temp
                         score += temp
                         score -= hlth
-                        #print "Score = %d" % score
 
     except Exception as e:
         print e
@@ -193,8 +168,6 @@ def evaluate(board, isplayer):
         exit()
 
     return(score)
-
-# In[11]:
 
 def environEffect(board):
     remainMem = copy.deepcopy(board[1:])
@@ -226,41 +199,24 @@ def environEffect(board):
     board[2] = copy.deepcopy(remainMem[1])
 
 def checkMove(oldBoard, idxUser, startPos, finishPos, plantTree = 0):
-
     try:
 
         deg = copy.deepcopy(finishPos)
-        #print "Deg is %r" % deg
         board = copy.deepcopy(oldBoard)
         neighbors = getReach2Cells(board[0], startPos[0], startPos[1])
-        #print "Neighbors are %r" % neighbors
         u = copy.deepcopy(finishPos)[0:2]
-        #print(board[0][startPos[0]][startPos[1]]["idx"],board[0][finishPos[0]][finishPos[1]]["idx"] , idxUser)
-        #print "u is %r" % u
         if (board[0][startPos[0]][startPos[1]]["idx"] == board[0][finishPos[0]][finishPos[1]]["idx"]):
-            #print "board index matched"
-            #print(board[0][startPos[0]][startPos[1]]["idx"],board[0][finishPos[0]][finishPos[1]]["idx"] , idxUser)
-            #if (idxUser == "Native"):
-                #print "Hitting one's own kin!"
             return (oldBoard, False)
         if deg in neighbors:
-            #print "deg is in neighbors"
             if deg[2] == 1:
-                #if (idxUser == "Native"):
-                    #print "deg[2] is 1"
                 if (board[0][finishPos[0]][finishPos[1]]["idx"] == idx["Blank"]):
-                    #print "board pos is blank"
                     board[0][finishPos[0]][finishPos[1]] = copy.deepcopy(board[0][startPos[0]][startPos[1]])
                     board[0][startPos[0]][startPos[1]] = copy.deepcopy(blank)
                     board[idx[idxUser]-2].remove(startPos)
                     board[idx[idxUser]-2].append(u)
                     return (board, True)
-                #print("Error case")
-                #print(board[0][startPos[0]][startPos[1]]["idx"],board[0][finishPos[0]][finishPos[1]]["idx"] , idxUser)
                 board[0][finishPos[0]][finishPos[1]]["health"] -= hit[idxUser][idxrev[board[0][finishPos[0]][finishPos[1]]["idx"]]]
                 if (board[0][finishPos[0]][finishPos[1]]["health"] <= 0):
-                    #board[0][finishPos[0]][finishPos[1]]["health"] = 0
-                    #print "cell ltez"
                     if board[0][u[0]][u[1]]["idx"] == 3 or board[0][u[0]][u[1]]["idx"] == 4:
                         if u in board[1]:
                             board[1].remove(u)
@@ -268,21 +224,13 @@ def checkMove(oldBoard, idxUser, startPos, finishPos, plantTree = 0):
                             board[2].remove(u)
                     board[0][finishPos[0]][finishPos[1]] = copy.deepcopy(board[0][startPos[0]][startPos[1]])
                     board[0][startPos[0]][startPos[1]] = copy.deepcopy(blank)
-                    #print("u: " , idxUser)
-                    #print(board[idx[idxUser]-2])
-                    #print(startPos)
                     board[idx[idxUser]-2].remove(startPos)
                     board[idx[idxUser]-2].append(u)
 
-            else: #degree is 2
-                #print "deg[2] is not 1"
+            else:
                 if (board[0][finishPos[0]][finishPos[1]]["idx"] == idx["Blank"]):
-                    #if (idxUser == "Native"):
-                        #print ("Hitting far off empty\n")
                     return (oldBoard, False)
-                #print "pos is not blank"
                 board[0][finishPos[0]][finishPos[1]]["health"] -= hit[idxUser][idxrev[board[0][finishPos[0]][finishPos[1]]["idx"]]]
-                #print "idxrev", idxrev[board[0][finishPos[0]][finishPos[1]]["idx"]]
                 if (board[0][finishPos[0]][finishPos[1]]["health"] <= 0):
                     board[0][finishPos[0]][finishPos[1]] = copy.deepcopy(blank)
                     if board[0][u[0]][u[1]]["idx"] == 3 or board[0][u[0]][u[1]]["idx"] == 4:
@@ -310,14 +258,9 @@ def checkMove(oldBoard, idxUser, startPos, finishPos, plantTree = 0):
             print [x["health"] for x in row]
         raise
         exit()
-    #if (idxUser == "Native"):
-        #print "Don't know why"
     print "deg ", deg
     print "neighbors ", neighbors
     return (oldBoard, False)
-
-# In[12]:
-
 
 def init():
     board = []
@@ -347,38 +290,22 @@ def init():
     return board
 
 
-# In[14]:
-
 def MaxValuePoacher (board, depth):
-    #print "MaxValuePoacher"
-    #for row in board[0]:
-    #    print ([(x["idx"], x["health"]) for x in row])
-    #print board[1:3]
     if depth > n["depMiniMax"]:
         return (evaluate(board[0], True), evaluate(board[0], False))
     maxim = float("-inf")
     corrOppScore = maxim
     poachers = board[2]
     for h in poachers:
-        #print("tmc1: ", h)
         if h not in board[2]:
             continue
         places = getReach2Cells (board[0], h[0], h[1])
         for move in places:
-            #for row in board[0]:
-                #print ([(x["idx"], x["health"]) for x in row])
-            #print("Natives: ", board[1])
-            #print("Poachers: ", board[2])
-            #print("tc1: ", h, h[0], h[1], board[0][h[0]][h[1]]["idx"])
-            #print "At MaxValPoacher"
-            #printBoard(board)
             if h not in board[2]:
                 continue
             newBoard, success = checkMove(board, "Poacher", [h[0], h[1]], move)
             if (success):
-                #print "Move made %r %r" %( h, move)
                 plScore, poScore = MaxValuePlayer (newBoard, depth + 1)
-                #print "lala"
                 if (poScore > maxim):
                     if depth == 0:
                         nextBoard = copy.deepcopy(newBoard)
@@ -392,34 +319,19 @@ def MaxValuePoacher (board, depth):
     return (maxim, corrOppScore)
 
 def MaxValuePlayer (board, depth):
-    #print "MaxValuePlayer"
-
-        #print "%r, %r" % ([x["idx"] for x in row], [x["health"] for x in row])
-        #print ([(x["idx"], x["health"]) for x in row])
-    #print board[1:3]
     maxim = float("-inf")
     corrOppScore = maxim
     natives = board[1]
     for h in natives:
         if h not in board[1]:
             continue
-        #print("tmc2: ", h)
         places = getReach2Cells (board[0], h[0], h[1])
         for move in places:
-            #for row in board[0]:
-                #print ([(x["idx"], x["health"]) for x in row])
-            #print("Natives: ", board[1])
-            #print("Poachers: ", board[2])
-            #print("tc2: ",h,  h[0], h[1], board[0][h[0]][h[1]]["idx"])
-            #print "At MaxValPlayer"
-            #printBoard(board)
             if h not in board[1]:
                 continue
             newBoard, success = checkMove(board, "Native", [h[0], h[1]], move)
             if (success):
-                #print "Move made %r %r"%( h, move)
                 poScore, plScore = MaxValuePoacher (newBoard, depth + 1)
-                #print "mama"
                 if (plScore > maxim):
                     maxim = plScore
                     corrOppScore = poScore
@@ -457,10 +369,7 @@ def asciigame():
         for u in r:
             if (u[0:2] == finishPos):
                 finishPos = copy.deepcopy(u)
-                #print finishPos
                 break
-        #plantTree = int(raw_input())
-        #print finishPos
         newBoard, success = checkMove(board, "Native", startPos, finishPos)
         if (not success):
             print ("\nIllegal move for some reason! Please try again\n\n")
